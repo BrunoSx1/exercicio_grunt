9f4fe6a4ecbf378b73e9b18d519ed00cd7af8e6f
BrunoSx1 <s.bruno.12@hotmail.com>
2023-02-02 06:58:51 -0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+        },
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'js/script.min.js': ['js/script.js']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['less', 'uglify']);
+};
